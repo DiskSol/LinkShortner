8226d733713a7f576238d1c5dea46fada828498b
Mosab Azizi <32365782+MosabDev@users.noreply.github.com>
2017-11-16 12:37:49 +0300

    Update index2.php

--- index2.php
+++ index2.php
@@ -77,7 +77,7 @@ if (!$link) {
 //echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
 //echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
 
-mysqli_close($link);
+//mysqli_close($link);
 		    
 		    
 		    
