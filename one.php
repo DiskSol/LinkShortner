c585d45b4ff4a5272881ca0b2a243819a0e57e58
Mosab Azizi <32365782+MosabDev@users.noreply.github.com>
2017-11-16 10:33:45 +0300

    Update one.php

--- one.php
+++ one.php
@@ -14,13 +14,43 @@ if (isset($_POST['curl'])) {
 
 else{
 
-  $db_server = '';
-  $db_user_name = '';
-  $db_password = '';
-  $db_name = '';
-  $_SESSION['count']=0;
-  @ $db = mysqli_connect($db_server,$db_user_name,$db_password,$db_name) or die("unable to connect to database");
+  $connectstr_dbhost = '';
+$connectstr_dbname = '';
+$connectstr_dbusername = '';
+$connectstr_dbpassword = '';
 
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
+    }
+    
+    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+
+$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+mysqli_close($link);
+    
+    
+    
+    
+    
+    
+    
+    
 
   $raw_results = mysqli_query($db,"SELECT * FROM t1
             WHERE (`longg` LIKE '%".$curl1."%')") ;
