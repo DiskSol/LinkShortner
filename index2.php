f60e7ee7415357ed7be0f9c2fc4ec67d02a09db6
Mosab Azizi <32365782+MosabDev@users.noreply.github.com>
2017-11-16 13:12:10 +0300

    change to clearDB from IBM

--- index2.php
+++ index2.php
@@ -49,10 +49,10 @@ function ClipBoard()
             <?php
             if(isset($_GET['yes'])) {
 
-              $db_server = 'localhost';
-              $db_user_name = 'user';
-              $db_password = '123456';
-              $db_name = 'url';
+                $db_server = 'us-cdbr-sl-dfw-01.cleardb.net';
+  $db_user_name = 'b2e0961d6fc128';
+  $db_password = 'cdbcaad1';
+  $db_name = 'ibmx_ca810b7837fe5f8';
               $suff1 =$_SESSION ['user'];
               @ $db = mysqli_connect($db_server,$db_user_name,$db_password,$db_name) or die("unable to connect to database");
 
